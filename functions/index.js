a423cbd57c9230b2bc41a5238fd8b9cfc2170769
Michael Phelps <michaeljphelps1978@gmail.com>
2021-01-16 17:55:50 -0600

    :sparkles: Basic Signup
    Signup integrated with Google Authentication - returns access token, and is unique

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,27 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const app = require('express')()
 
 admin.initializeApp();
 
-const express = require('express')
-const app = express()
+const config = {
+  apiKey: "AIzaSyBvgdEXo3WWapvVLqO-OdA6SriH1gK3Bvs",
+  authDomain: "socialmediaclone-f5653.firebaseapp.com",
+  projectId: "socialmediaclone-f5653",
+  storageBucket: "socialmediaclone-f5653.appspot.com",
+  messagingSenderId: "586861615492",
+  appId: "1:586861615492:web:5708db2b7ae6dd9a07a5d1"
+}
 
 
+const firebase = require('firebase')
+firebase.initializeApp(config)
 
+const db = admin.firestore()
 
 // This route will pull from our posts database
 app.get('/blasts', (req, res) => {
-  admin
+  db
   .firestore()
   .collection("blasts")
   .orderBy('createdAt', 'desc')
@@ -48,7 +58,7 @@ app.post('/blasts',(req, res) => {
   };
 
   // add the new post object as a fireStore document
-  admin
+  db
     .firestore()
     .collection("blasts")
     .add(newBlast)
@@ -63,6 +73,43 @@ app.post('/blasts',(req, res) => {
       console.error(err);
     });
 });
+
+// user sign up
+
+app.post('/signup', (req,res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    userName: req.body.userName,
+  }
+
+  //TODO: Validate Data
+
+  db.doc(`/users/${newUser.userName}`).get()
+  .then(doc => {
+    if (doc.exists) {
+      return res.status(400).json({userName: 'This userName is already taken'})
+    } else {
+      return firebase
+      .auth()
+      .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    }
+  })
+  .then(data => {
+    return data.user.getIdToken()
+  })
+  .then(token => {
+    return res.status(201).json({ token })
+  })
+  .catch(err => {
+    if (err.code === 'auth/email-already-in-use') {
+      return res.status(400).json({email: 'Email already in use'})
+    } else {
+      return res.status(500).json({ error: err.code})
+    }
+  })
+})
 
 
 // create our generic route (https://baseurl.com/api/)
