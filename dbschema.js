b7c67e68a419c2ec79407b7b8365717770f84122
Michael Phelps <michaeljphelps1978@gmail.com>
2021-01-16 17:10:09 -0600

    :tada: Initial Commit
    API Endpoints for view and create posts (fireBase)

---
+++ dbschema.js
@@ -0,0 +1,15 @@
+let db = {
+
+  blasts: [
+
+    {
+      userName: 'user',
+      body: 'this is the blast body',
+      createdAt: '2021-01-16T23:04:23.377Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+
+  ]
+
+}
