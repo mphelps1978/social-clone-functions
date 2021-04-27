535cafcee2cd47398f81dc1dc961146a5aae4afb
Michael Phelps <michaeljphelps1978@gmail.com>
2021-04-27 10:16:43 -0500

    Adds Env Variables for security

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,10 +1,10 @@
 const { storage } = require('firebase-admin');
 
 module.exports = {
-  apiKey: "AIzaSyBvgdEXo3WWapvVLqO-OdA6SriH1gK3Bvs",
-  authDomain: "socialmediaclone-f5653.firebaseapp.com",
-  projectId: "socialmediaclone-f5653",
-  storageBucket: "socialmediaclone-f5653.appspot.com",
-  messagingSenderId: "586861615492",
-  appId: "1:586861615492:web:5708db2b7ae6dd9a07a5d1",
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
 };
